e9097141c63915e65e44aae2855f419fba952333
remyp2012 <renyipeter@hotmail.com>
2014-04-05 17:24:36 +0200

    add again vector

--- src/tikai/util.java
+++ src/tikai/util.java
@@ -1,6 +1,6 @@
 package tikai;
 
 public class util {
-	double[] dxs = {Math.cos(Math.PI * 1/10), Math.cos(Math.PI * 3/10), Math.cos(Math.PI * 1/2), Math.cos(Math.PI * 5/6), Math.cos(Math.PI * 7/6), Math.cos(Math.PI *3/2), Math.cos(Math.PI *17/10), Math.cos(Math.PI *19/10)};
-	double[] dys = {Math.sin(Math.PI * 1/10), Math.sin(Math.PI * 3/10), Math.sin(Math.PI * 1/2), Math.sin(Math.PI * 5/6), Math.sin(Math.PI * 7/6), Math.sin(Math.PI *3/2), Math.sin(Math.PI *17/10), Math.sin(Math.PI *19/10)};
+	final double[] dxs = {Math.cos(Math.PI * 1/6), Math.cos(Math.PI * 1/2), Math.cos(Math.PI * 7/10), Math.cos(Math.PI * 9/10), Math.cos(Math.PI * 11/10), Math.cos(Math.PI *13/10), Math.cos(Math.PI *3/2), Math.cos(Math.PI *11/6)};
+	final double[] dys = {Math.sin(Math.PI * 1/6), Math.sin(Math.PI * 1/2), Math.sin(Math.PI * 7/10), Math.sin(Math.PI * 9/10), Math.sin(Math.PI * 11/10), Math.sin(Math.PI *13/10), Math.sin(Math.PI *3/2), Math.sin(Math.PI *11/6)};
 }
