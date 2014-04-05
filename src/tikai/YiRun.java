fb0106878da80569f3da3a030962038079f52d3d
remyp2012 <renyipeter@hotmail.com>
2014-04-05 17:13:14 +0200

    add car init vector

--- src/tikai/YiRun.java
+++ src/tikai/YiRun.java
@@ -1,13 +1,13 @@
 package tikai;
 
-public class TikaiRun {
+public class YiRun {
 
 	public static void main(String[] args) {
-		Enonce enonce= new Enonce("/Users/peter/Desktop/streetview/paris_54000.txt");
+		Enonce enonce= new Enonce("/home/teacup123123/Desktop/paris_54000.txt");
 		OutPut out=new OutPut(enonce);
 //		lots of out.moveCar(indexCar, newJunctionIndex);...
 		out.algoTikai();
-		out.outPutToFile("/Users/peter/Desktop/streetview/result.txt");
+		out.outPutToFile("/home/teacup123123/Desktop/result.txt");
 	}
 
 }
