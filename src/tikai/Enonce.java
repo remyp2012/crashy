0535dddee78f25af219a5feb6e4a93b4f3a11886
teacup123123 <teacup123123@gmail.com>
2014-04-05 12:17:10 +0200

    input enonce

--- src/tikai/Enonce.java
+++ src/tikai/Enonce.java
@@ -2,8 +2,12 @@ package tikai;
 
 import iotools.ReadTool;
 
+import java.util.ArrayList;
+
 public class Enonce {
 	public final int nbJunc, nbStr,nbT,nbCar,indexStart;
+	public final ArrayList<Junction> juncList=new ArrayList<Junction>();
+	public final ArrayList<Street> streetList=new ArrayList<Street>();
 	public Enonce(String str) {
 		ReadTool read=new ReadTool(str);
 		read.readLine();
@@ -12,8 +16,27 @@ public class Enonce {
 		nbT=Integer.parseInt(read.segment(2));
 		nbCar=Integer.parseInt(read.segment(3));
 		indexStart=Integer.parseInt(read.segment(4));
-		
-		
-		
+
+
+		for(int i=0;i<nbJunc;++i)
+		{
+			read.readLine();
+			juncList.add(
+					new Junction(Double.parseDouble(read.segment(0)),Double.parseDouble(read.segment(1)))
+					);
+		}
+		for(int i=0;i<nbStr;++i)
+		{
+			read.readLine();
+			streetList.add(
+					new Street(Integer.parseInt(read.segment(0)),
+							Integer.parseInt(read.segment(2)),
+							(Integer.parseInt(read.segment(1))==2),
+							Integer.parseInt(read.segment(3)),
+							Integer.parseInt(read.segment(4))
+							)
+					);
+		}
+
 	}
 }
